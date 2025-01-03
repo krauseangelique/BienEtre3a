ec80e3899142970a4df49e9065282bd0843852ab
angelique KRAUSE <krause_angelique@hotmail.com>
2025-01-03 20:24:48 +0100

    mise en place base header et footer

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    //  Pour réécrire l'url de /home en https://127.0.0.1:8000/ plus joliment écrit / puis actualiser dans le navigateur
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
+    {
+        $name = "Angelique";
+
+        return $this->render('home/index.html.twig', [
+
+            // Tableau associatif les variables twig première partie pourront être retournées
+            'controller_name' => 'HomeController',
+            'My_name' => $name
+        ]);
+    }
+}
